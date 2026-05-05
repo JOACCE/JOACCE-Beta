25f52078e3624075c8e8d3b652945f4742abadb7
Connor-Villanueva <ckvillan@calpoly.edu>
2026-05-04 19:15:36 -0700

    Added black background png

--- joacce/scenes/camera_input/camera_input.gd
+++ joacce/scenes/camera_input/camera_input.gd
@@ -6,20 +6,37 @@ var camera: CameraFeed
 @onready var display: Sprite2D = $Sprite2D
 
 func _ready() -> void:
-	# Allow CameraServer monitor feeds
+	# Allow CameraServer to monitor feeds
 	CameraServer.monitoring_feeds = true
 	
+	var cam: CameraFeed = null
+	
 	# Get currently connected camera feeds
-	# Will need player to choose from these options
+	# Will need player to choose from these options	
 	for feed in CameraServer.feeds():
-		var name = feed.get_name()
+		var name : String = feed.get_name()
 		
-		if (camera == null and name == camera_name):
-			camera = feed
+		# Right now just picking a set camera
+		if (cam == null and name == camera_name):
+			cam = feed
 			break
 	
-	print("Using camera: ", camera.get_name())
+	_turn_on_camera_feed(cam)
+	
+func _turn_on_camera_feed(cam: CameraFeed):
+	# Turn off existing camera if exists
+	if (camera):
+		camera.feed_is_active = false
+
+	# If no camera, do nothing
+	if (!cam):
+		return
+
+	# Set and turn on camera feed
+	camera = cam
 	camera.feed_is_active = true
+	
+	print("Using Camera: ", camera.get_name())
 	
 	var cam_tex_y = display.material.get_shader_parameter("camera_y")
 	var cam_tex_CbCr = display.material.get_shader_parameter("camera_CbCr")
