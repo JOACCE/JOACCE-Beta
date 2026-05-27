e05dc06b52cb94be6211048bfc9bd464ccb6bf3d
Chinmay Karur <ckarur@calpoly.edu>
2026-05-27 15:40:26 -0700

    Added play area, fight ring, improved camera transition, added lighting and world env

--- joacce/scripts/intro/intro_scene.gd
+++ joacce/scripts/intro/intro_scene.gd
@@ -2,12 +2,17 @@ extends Node3D
 
 @export var path_follow: PathFollow3D
 @export var title_label: Label
+@export var character_label: Label
 @export var duration: float = 16.0
 
 func _ready() -> void:
 	path_follow.progress_ratio = 0.0
+
 	title_label.modulate.a = 0.0
 	title_label.visible = true
+
+	character_label.modulate.a = 0.0
+	character_label.visible = true
 
 	var camera_tween = create_tween()
 	camera_tween.tween_property(
@@ -43,4 +48,16 @@ func _ready() -> void:
 
 	title_tween.tween_callback(func():
 		title_label.visible = false
+	)
+
+	# Wait until camera reaches end of path
+	var character_tween = create_tween()
+	character_tween.tween_interval(duration)
+
+	# Fade in character select text
+	character_tween.tween_property(
+		character_label,
+		"modulate:a",
+		1.0,
+		0.1
 	)
