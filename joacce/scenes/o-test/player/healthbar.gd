54bcd77b02a6f1220d10ff7a49fa5983f02d6b16
jmgdls <jazzymae.delossantos@gmail.com>
2026-06-03 17:23:43 -0700

    update health bar and charge meter

--- joacce/scenes/o-test/player/healthbar.gd
+++ joacce/scenes/o-test/player/healthbar.gd
@@ -1,40 +1,32 @@
-extends ProgressBar
-
-@onready var timer = $DamageTimer
-@onready var damage_bar = $DamageBar
+extends TextureProgressBar
 
 # Will be synced to each player later
 # : set calls the given function for every value change
+
 var health = 0 : set = _set_health
+var _updating := false
 
 func _ready() -> void:
 	init_health(100)
-
+	
 # Set initial health state for UI
 func init_health(in_health: float) -> void:
+	max_value = in_health
 	health = in_health
-	max_value = health
 	# Set progress bar value (initially matches in damagebar)
 	value = health
-	damage_bar.max_value = max_value
-	damage_bar.value = health
-
+	
+	
 # Runs on event health variable changes value
 func _set_health(new_health: float) -> void:
-	var prev_health = health
-	health = min(max_value, new_health)
+	if _updating:
+		return
+	_updating = true
+	health = clamp(new_health, 0, max_value)
 	value = health
+	_updating = false
+	print("current health: ", health)
 	
-	if health < prev_health:
-		timer.start()
-	else:
-		# Case of added health
-		damage_bar.value = value
-
-# Once timer ends set bar behind healthbar
-func _on_damage_timer_timeout() -> void:
-	damage_bar.value = health
-
 # TEMPORARY FOR TESTING, pressing reduces health by 10
 func _on_button_pressed() -> void:
 	health -= 10
