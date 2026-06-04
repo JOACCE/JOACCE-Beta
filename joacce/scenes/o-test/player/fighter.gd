3b31c91f8d0cbecc9f932d5629a6d34ace503c1b
Ozcar Cayetano <olopez2015.ol@gmail.com>
2026-06-03 20:25:28 -0700

    changed knockback to feel a little better

--- joacce/scenes/o-test/player/fighter.gd
+++ joacce/scenes/o-test/player/fighter.gd
@@ -98,7 +98,7 @@ func apply_damage(damage) -> void:
 	health_bar.health = health
 
 func _on_hitarea_body_entered(body: Node2D) -> void:
-	if name != body.name:
+	if name != body.name and body.has_method("apply_damage"):
 		body.apply_damage(5)
 
 func play_special(sprite: AnimatedSprite2D) -> void:
