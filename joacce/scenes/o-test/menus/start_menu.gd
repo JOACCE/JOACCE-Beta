daa6efb76b94d6dd8222cf4415d0a264c3664dae
OzcarC <olopez2015.ol@gmail.com>
2026-06-12 19:51:28 -0700

    Deletion through main menu

--- joacce/scenes/o-test/menus/start_menu.gd
+++ joacce/scenes/o-test/menus/start_menu.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var popup : CenterContainer = $CenterContainer
 @onready var popup_label : Label = $CenterContainer/VBoxContainer/Label
 
+@onready var deletion: Control = $Deletion
+@onready var char_list: ItemList = $Deletion/CharList
+
 # Menu Buttons
 @onready var fight_button : Button = $VBoxContainer/Fight
 @onready var create_button : Button = $VBoxContainer/Create
@@ -15,13 +18,22 @@ func _on_fight_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/character-select/selection.tscn")
 
 func _on_create_pressed() -> void:
+	var chars = CharacterManager.get_all_characters()
+	var char_count = chars.size()
+	print(char_count)
 	if (OS.get_name() == "Web"):
-		get_tree().change_scene_to_file("res://scenes/character_creation/camera_capture_web/camera_input.tscn")
+		if char_count >= 9:
+			prompt_char_deletion()
+		else:
+			get_tree().change_scene_to_file("res://scenes/character_creation/camera_capture_web/camera_input.tscn")
 	#elif (OS.get_name() == "Windows"):
 		#get_tree().change_scene_to_file("res://scenes/character_creation/camera_capture_windows/camera_input.tscn")
 	elif (OS.get_name() in ["macOS", "Linux", "Android", "iOS"]):
 		# Camera selection works with Goot CameraServer
-		get_tree().change_scene_to_file("res://scenes/character_creation/camera_select/camera_select.tscn")
+		if char_count >= 9:
+			prompt_char_deletion()
+		else:
+			get_tree().change_scene_to_file("res://scenes/character_creation/camera_select/camera_select.tscn")
 	else:
 		_disable_menu_buttons()
 		popup_label.text = "Character creation not supported on " + OS.get_name()
@@ -63,3 +75,26 @@ func _on_quit_mouse_entered() -> void:
 
 func _on_dismiss_button_mouse_entered() -> void:
 	AudioManager.play_hover_sfx()
+	
+func prompt_char_deletion():
+	_disable_menu_buttons()
+	var customs = CharacterManager.get_customs()
+	char_list.clear()
+	for custom_char in customs:
+		char_list.add_item(custom_char.name, custom_char.idle)
+	deletion.show()
+	
+
+
+func _on_del_confirm_button_pressed() -> void:
+	AudioManager.play_click_sfx()
+	_enable_menu_buttons()
+	deletion.hide()
+	var selected_idx = char_list.get_selected_items()[0]
+	var selected_name = char_list.get_item_text(selected_idx)
+	CharacterManager.delete(selected_name)
+
+func _on_del_dismiss_button_pressed() -> void:
+	AudioManager.play_click_sfx()
+	_enable_menu_buttons()
+	deletion.hide()
